b1ce3bcac1f06ecebb1edca0b701b6de30aaf4c4
Rahul Kundu <55733593+rahul8436@users.noreply.github.com>
2021-08-18 19:27:27 +0530

    Create q4.js

---
+++ q4.js
@@ -0,0 +1,17 @@
+function foo1()
+{
+return {
+    bar: "hello"
+    };
+}
+function foo2(){
+    return
+    {
+    bar: "hello"
+    };
+}
+/*********ANSWER********/
+
+
+//foo1() will be printing [object Object]
+//foo2() will pring nothing in the console
