4a196e6bbd586eb03c9c504d507c209266c4850a
Rahul Kundu <55733593+rahul8436@users.noreply.github.com>
2021-08-18 19:47:20 +0530

    Update q6.js

--- q6.js
+++ q6.js
@@ -2,3 +2,6 @@ const set = new Set(['Beethoven', 'Mozart', 'Chopin', 'Chopin'])
 //console.log(set.size)
 set.delete('Beethoven');
 //console.log(set.size)
+set. forEach((i) => {
+console.log(i);
+});
