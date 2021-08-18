694a921c964f5176d23dfcac0ceaeaf38e9f62d8
Rahul Kundu <55733593+rahul8436@users.noreply.github.com>
2021-08-18 19:28:40 +0530

    Create q7.js

---
+++ q7.js
@@ -0,0 +1,25 @@
+const  operatingSystem = { 
+    name: 'Ubuntu',
+    version:'18.4' , 
+    license: 'open source'
+};
+//can also be written as
+
+const  operatingSystem = [ 
+    {
+        name: 'Ubuntu',
+        version:'18.4' , 
+        license: 'open source'
+    },
+    //adding some arbitary values
+    {
+        name: 'Ubuntu',
+        version:'18.4' , 
+        license: 'open source'
+    },
+    {
+        name: 'Ubuntu',
+        version:'18.4' , 
+        license: 'open source'
+    }
+];
